85c721bf4413c6e7ae4c0acafebd001fa00b00b6
Alina <116342238+Alina-02@users.noreply.github.com>
2023-09-15 11:41:13 +0200

    Delete src/firebase_constants.js

--- src/firebase_constants.js
+++
@@ -1,6 +0,0 @@
-export const API_KEY = "AIzaSyBDWx61rZktleXwnnvh2Agl_69yqTAIX3M";
-export const AUTH_DOMAIN = "booknote-67b71.firebaseapp.com";
-export const PROJECT_ID = "booknote-67b71";
-export const STORAGE_BUCKET = "booknote-67b71.appspot.com";
-export const MESSAGING_SENDER_ID = "419625535077";
-export const APP_ID = "1:419625535077:web:1565ed855663b39b24688c";
